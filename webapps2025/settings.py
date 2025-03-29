39978b96bfc880f3731edd0487fde5006a3d0d7f
Brian Rapudo Ochieng <rapudobrian@gmail.com>
2025-03-29 09:23:02 +0300

    updated projet structure

--- webapps2025/settings.py
+++ webapps2025/settings.py
@@ -76,7 +76,8 @@ WSGI_APPLICATION = 'webapps2025.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': BASE_DIR / "webapps.db",
+        # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
 
     }
 }
